74a11bf7f6681f01171e324ce527705e467b2b1d
Stanislav Mironov <mirst33@icloud.com>
2024-07-12 15:39:43 +0300

    add method update

--- lib/feature/home/friends/data/friend_data_source.dart
+++ lib/feature/home/friends/data/friend_data_source.dart
@@ -1,27 +1,34 @@
+import 'dart:core';
 import 'package:drift/drift.dart';
 import 'package:friends_list/core/components/database/database.dart';
 import 'package:friends_list/core/components/database/src/app_database.dart';
+import 'package:friends_list/core/components/database/src/tables/friends_table.dart';
 part "friend_data_source.g.dart";
 
 abstract class FriendDataSource {
-  Future<Friend> create({
+  Future<Friend> createFriend({
     required String name,
     required String lastName,
   });
   Future<int> deleteFriend(int id);
   Future<List<Friend>> getAllFriends();
+  Future<int> updateFriend({
+    required int id,
+    required String name,
+    required String lastName,
+  });
 }
 
-@DriftAccessor(tables: [AppDatabase])
+@DriftAccessor(tables: [Friends])
 class FriendDao extends DatabaseAccessor<AppDatabase>
     with _$FriendDaoMixin
     implements FriendDataSource {
   FriendDao(super.db);
 
   @override
-  Future<Friend> create({
-    required name,
-    required lastName,
+  Future<Friend> createFriend({
+    required String name,
+    required String lastName,
   }) async {
     return await db.into(db.friends).insertReturning(FriendsCompanion.insert(
           name: name,
@@ -31,9 +38,19 @@ class FriendDao extends DatabaseAccessor<AppDatabase>
 
   @override
   Future<int> deleteFriend(int id) async {
-    // Delete user with id 1
     return await (db.delete(db.friends)..where((tbl) => tbl.id.equals(id)))
         .go();
+  }
+
+  @override
+  Future<int> updateFriend({
+    required int id,
+    required String name,
+    required String lastName,
+  }) async {
+// Conditional update for users
+    return await (db.update(db.friends)..where((tbl) => tbl.id.equals(id)))
+        .write(FriendsCompanion(name: Value(name), lastName: Value(lastName)));
   }
 
   @override

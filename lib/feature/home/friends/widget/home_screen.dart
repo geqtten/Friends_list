7325939f7bbe0249cae2c944a1abecd57fdfe475
Stanislav Mironov <mirst33@icloud.com>
2024-07-16 17:17:58 +0300

    add bottomNavBar

--- lib/feature/home/friends/widget/home_screen.dart
+++ lib/feature/home/friends/widget/home_screen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:friends_list/feature/home/friends/logic/bloc/friends_bloc.dart';
-import 'package:friends_list/feature/home/friends/logic/bloc/friends_event.dart';
-import 'package:friends_list/feature/home/friends/logic/bloc/friends_state.dart';
-import 'package:friends_list/feature/home/friends/widget/components/friend_card.dart';
-import 'package:friends_list/feature/initialization/widget/dependencies_scope.dart';
+import 'package:friends_list/feature/home/friends/widget/components/settings_screen.dart';
+import 'package:friends_list/feature/home/friends/widget/friends_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,9 +11,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late final FriendBloc friendBloc;
-  late bool isSearch;
   int _selectedIndex = 0;
+
+  static const List<Widget> screens = <Widget>[
+    FriendsScreen(),
+    SettingsScreen(),
+  ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -25,99 +25,26 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    isSearch = false;
-    friendBloc = FriendBloc(
-        friendRepository: DependenciesScope.of(context).friendRepository)
-      ..add(const FriendEvent.loadAllFriends());
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: BlocProvider.value(
-        value: friendBloc,
-        child: Scaffold(
-          appBar: AppBar(
-            title: !isSearch ? const Text("Friends List") : null,
-            backgroundColor: Colors.grey[400],
-            actions: [
-              if (isSearch) ...[
-                const Icon(
-                  Icons.person,
-                  size: 35,
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                const _Search()
-              ],
-              IconButton(
-                icon: !isSearch
-                    ? const Icon(Icons.search)
-                    : const Icon(Icons.cancel_outlined),
-                onPressed: () {
-                  setState(() {
-                    isSearch = !isSearch;
-                  });
-                },
-              ),
-            ],
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey[400],
+        iconSize: 30,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Friends',
           ),
-          body: BlocBuilder<FriendBloc, FriendState>(
-            builder: (context, state) => Center(
-              child: ListView.builder(
-                itemCount: state.friends.length,
-                itemBuilder: (context, index) => FriendCard(
-                  state.friends[index].name,
-                  state.friends[index].lastName,
-                  state.friends[index].id,
-                  friendBloc,
-                ),
-              ),
-            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
           ),
-          floatingActionButton: const CreateCard(),
-          bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.grey[400],
-            iconSize: 30,
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Friends',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                label: 'Settings',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.black87,
-            onTap: _onItemTapped,
-          ),
-        ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black87,
+        onTap: _onItemTapped,
       ),
-    );
-  }
-}
-
-class _Search extends StatelessWidget {
-  const _Search();
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(
-      height: 30,
-      width: 300,
-      child: TextField(
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: "Search",
-          fillColor: Colors.black12,
-          filled: true,
-        ),
-      ),
+      body: screens.elementAt(_selectedIndex),
     );
   }
 }

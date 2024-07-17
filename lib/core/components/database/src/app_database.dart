ef8f2e9b195bd72190b84b34b3a52fbef85cde9c
Stanislav Mironov <mirst33@icloud.com>
2024-07-17 21:34:17 +0300

    correction

--- lib/core/components/database/src/app_database.dart
+++ lib/core/components/database/src/app_database.dart
@@ -1,10 +1,11 @@
 import 'package:drift/drift.dart';
 import 'package:friends_list/core/components/database/database.dart';
 import 'package:friends_list/core/components/database/src/tables/friends_table.dart';
+import 'package:friends_list/core/components/database/src/tables/users_table.dart';
 
 part 'app_database.g.dart';
 
-@DriftDatabase(tables: [Friends])
+@DriftDatabase(tables: [Friends, Users])
 class AppDatabase extends _$AppDatabase {
   AppDatabase([QueryExecutor? executor]) : super(executor ?? createExecutor());
 
--- lib/core/components/database/src/app_database.g.dart
+++ lib/core/components/database/src/app_database.g.dart
@@ -208,15 +208,262 @@ class FriendsCompanion extends UpdateCompanion<Friend> {
   }
 }
 
+class $UsersTable extends Users with TableInfo<$UsersTable, User> {
+  @override
+  final GeneratedDatabase attachedDatabase;
+  final String? _alias;
+  $UsersTable(this.attachedDatabase, [this._alias]);
+  static const VerificationMeta _idMeta = const VerificationMeta('id');
+  @override
+  late final GeneratedColumn<int> id = GeneratedColumn<int>(
+      'id', aliasedName, false,
+      hasAutoIncrement: true,
+      type: DriftSqlType.int,
+      requiredDuringInsert: false,
+      defaultConstraints:
+          GeneratedColumn.constraintIsAlways('PRIMARY KEY AUTOINCREMENT'));
+  static const VerificationMeta _loginMeta = const VerificationMeta('login');
+  @override
+  late final GeneratedColumn<String> login = GeneratedColumn<String>(
+      'login', aliasedName, false,
+      type: DriftSqlType.string, requiredDuringInsert: true);
+  static const VerificationMeta _passwordMeta =
+      const VerificationMeta('password');
+  @override
+  late final GeneratedColumn<String> password = GeneratedColumn<String>(
+      'password', aliasedName, false,
+      type: DriftSqlType.string, requiredDuringInsert: true);
+  static const VerificationMeta _isLoginMeta =
+      const VerificationMeta('isLogin');
+  @override
+  late final GeneratedColumn<int> isLogin = GeneratedColumn<int>(
+      'is_login', aliasedName, false,
+      type: DriftSqlType.int, requiredDuringInsert: true);
+  @override
+  List<GeneratedColumn> get $columns => [id, login, password, isLogin];
+  @override
+  String get aliasedName => _alias ?? actualTableName;
+  @override
+  String get actualTableName => $name;
+  static const String $name = 'users';
+  @override
+  VerificationContext validateIntegrity(Insertable<User> instance,
+      {bool isInserting = false}) {
+    final context = VerificationContext();
+    final data = instance.toColumns(true);
+    if (data.containsKey('id')) {
+      context.handle(_idMeta, id.isAcceptableOrUnknown(data['id']!, _idMeta));
+    }
+    if (data.containsKey('login')) {
+      context.handle(
+          _loginMeta, login.isAcceptableOrUnknown(data['login']!, _loginMeta));
+    } else if (isInserting) {
+      context.missing(_loginMeta);
+    }
+    if (data.containsKey('password')) {
+      context.handle(_passwordMeta,
+          password.isAcceptableOrUnknown(data['password']!, _passwordMeta));
+    } else if (isInserting) {
+      context.missing(_passwordMeta);
+    }
+    if (data.containsKey('is_login')) {
+      context.handle(_isLoginMeta,
+          isLogin.isAcceptableOrUnknown(data['is_login']!, _isLoginMeta));
+    } else if (isInserting) {
+      context.missing(_isLoginMeta);
+    }
+    return context;
+  }
+
+  @override
+  Set<GeneratedColumn> get $primaryKey => {id};
+  @override
+  User map(Map<String, dynamic> data, {String? tablePrefix}) {
+    final effectivePrefix = tablePrefix != null ? '$tablePrefix.' : '';
+    return User(
+      id: attachedDatabase.typeMapping
+          .read(DriftSqlType.int, data['${effectivePrefix}id'])!,
+      login: attachedDatabase.typeMapping
+          .read(DriftSqlType.string, data['${effectivePrefix}login'])!,
+      password: attachedDatabase.typeMapping
+          .read(DriftSqlType.string, data['${effectivePrefix}password'])!,
+      isLogin: attachedDatabase.typeMapping
+          .read(DriftSqlType.int, data['${effectivePrefix}is_login'])!,
+    );
+  }
+
+  @override
+  $UsersTable createAlias(String alias) {
+    return $UsersTable(attachedDatabase, alias);
+  }
+}
+
+class User extends DataClass implements Insertable<User> {
+  final int id;
+  final String login;
+  final String password;
+  final int isLogin;
+  const User(
+      {required this.id,
+      required this.login,
+      required this.password,
+      required this.isLogin});
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    map['id'] = Variable<int>(id);
+    map['login'] = Variable<String>(login);
+    map['password'] = Variable<String>(password);
+    map['is_login'] = Variable<int>(isLogin);
+    return map;
+  }
+
+  UsersCompanion toCompanion(bool nullToAbsent) {
+    return UsersCompanion(
+      id: Value(id),
+      login: Value(login),
+      password: Value(password),
+      isLogin: Value(isLogin),
+    );
+  }
+
+  factory User.fromJson(Map<String, dynamic> json,
+      {ValueSerializer? serializer}) {
+    serializer ??= driftRuntimeOptions.defaultSerializer;
+    return User(
+      id: serializer.fromJson<int>(json['id']),
+      login: serializer.fromJson<String>(json['login']),
+      password: serializer.fromJson<String>(json['password']),
+      isLogin: serializer.fromJson<int>(json['isLogin']),
+    );
+  }
+  @override
+  Map<String, dynamic> toJson({ValueSerializer? serializer}) {
+    serializer ??= driftRuntimeOptions.defaultSerializer;
+    return <String, dynamic>{
+      'id': serializer.toJson<int>(id),
+      'login': serializer.toJson<String>(login),
+      'password': serializer.toJson<String>(password),
+      'isLogin': serializer.toJson<int>(isLogin),
+    };
+  }
+
+  User copyWith({int? id, String? login, String? password, int? isLogin}) =>
+      User(
+        id: id ?? this.id,
+        login: login ?? this.login,
+        password: password ?? this.password,
+        isLogin: isLogin ?? this.isLogin,
+      );
+  @override
+  String toString() {
+    return (StringBuffer('User(')
+          ..write('id: $id, ')
+          ..write('login: $login, ')
+          ..write('password: $password, ')
+          ..write('isLogin: $isLogin')
+          ..write(')'))
+        .toString();
+  }
+
+  @override
+  int get hashCode => Object.hash(id, login, password, isLogin);
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is User &&
+          other.id == this.id &&
+          other.login == this.login &&
+          other.password == this.password &&
+          other.isLogin == this.isLogin);
+}
+
+class UsersCompanion extends UpdateCompanion<User> {
+  final Value<int> id;
+  final Value<String> login;
+  final Value<String> password;
+  final Value<int> isLogin;
+  const UsersCompanion({
+    this.id = const Value.absent(),
+    this.login = const Value.absent(),
+    this.password = const Value.absent(),
+    this.isLogin = const Value.absent(),
+  });
+  UsersCompanion.insert({
+    this.id = const Value.absent(),
+    required String login,
+    required String password,
+    required int isLogin,
+  })  : login = Value(login),
+        password = Value(password),
+        isLogin = Value(isLogin);
+  static Insertable<User> custom({
+    Expression<int>? id,
+    Expression<String>? login,
+    Expression<String>? password,
+    Expression<int>? isLogin,
+  }) {
+    return RawValuesInsertable({
+      if (id != null) 'id': id,
+      if (login != null) 'login': login,
+      if (password != null) 'password': password,
+      if (isLogin != null) 'is_login': isLogin,
+    });
+  }
+
+  UsersCompanion copyWith(
+      {Value<int>? id,
+      Value<String>? login,
+      Value<String>? password,
+      Value<int>? isLogin}) {
+    return UsersCompanion(
+      id: id ?? this.id,
+      login: login ?? this.login,
+      password: password ?? this.password,
+      isLogin: isLogin ?? this.isLogin,
+    );
+  }
+
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    if (id.present) {
+      map['id'] = Variable<int>(id.value);
+    }
+    if (login.present) {
+      map['login'] = Variable<String>(login.value);
+    }
+    if (password.present) {
+      map['password'] = Variable<String>(password.value);
+    }
+    if (isLogin.present) {
+      map['is_login'] = Variable<int>(isLogin.value);
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return (StringBuffer('UsersCompanion(')
+          ..write('id: $id, ')
+          ..write('login: $login, ')
+          ..write('password: $password, ')
+          ..write('isLogin: $isLogin')
+          ..write(')'))
+        .toString();
+  }
+}
+
 abstract class _$AppDatabase extends GeneratedDatabase {
   _$AppDatabase(QueryExecutor e) : super(e);
   _$AppDatabaseManager get managers => _$AppDatabaseManager(this);
   late final $FriendsTable friends = $FriendsTable(this);
+  late final $UsersTable users = $UsersTable(this);
   @override
   Iterable<TableInfo<Table, Object?>> get allTables =>
       allSchemaEntities.whereType<TableInfo<Table, Object?>>();
   @override
-  List<DatabaseSchemaEntity> get allSchemaEntities => [friends];
+  List<DatabaseSchemaEntity> get allSchemaEntities => [friends, users];
 }
 
 typedef $$FriendsTableInsertCompanionBuilder = FriendsCompanion Function({
@@ -321,9 +568,129 @@ class $$FriendsTableOrderingComposer
           ColumnOrderings(column, joinBuilders: joinBuilders));
 }
 
+typedef $$UsersTableInsertCompanionBuilder = UsersCompanion Function({
+  Value<int> id,
+  required String login,
+  required String password,
+  required int isLogin,
+});
+typedef $$UsersTableUpdateCompanionBuilder = UsersCompanion Function({
+  Value<int> id,
+  Value<String> login,
+  Value<String> password,
+  Value<int> isLogin,
+});
+
+class $$UsersTableTableManager extends RootTableManager<
+    _$AppDatabase,
+    $UsersTable,
+    User,
+    $$UsersTableFilterComposer,
+    $$UsersTableOrderingComposer,
+    $$UsersTableProcessedTableManager,
+    $$UsersTableInsertCompanionBuilder,
+    $$UsersTableUpdateCompanionBuilder> {
+  $$UsersTableTableManager(_$AppDatabase db, $UsersTable table)
+      : super(TableManagerState(
+          db: db,
+          table: table,
+          filteringComposer:
+              $$UsersTableFilterComposer(ComposerState(db, table)),
+          orderingComposer:
+              $$UsersTableOrderingComposer(ComposerState(db, table)),
+          getChildManagerBuilder: (p) => $$UsersTableProcessedTableManager(p),
+          getUpdateCompanionBuilder: ({
+            Value<int> id = const Value.absent(),
+            Value<String> login = const Value.absent(),
+            Value<String> password = const Value.absent(),
+            Value<int> isLogin = const Value.absent(),
+          }) =>
+              UsersCompanion(
+            id: id,
+            login: login,
+            password: password,
+            isLogin: isLogin,
+          ),
+          getInsertCompanionBuilder: ({
+            Value<int> id = const Value.absent(),
+            required String login,
+            required String password,
+            required int isLogin,
+          }) =>
+              UsersCompanion.insert(
+            id: id,
+            login: login,
+            password: password,
+            isLogin: isLogin,
+          ),
+        ));
+}
+
+class $$UsersTableProcessedTableManager extends ProcessedTableManager<
+    _$AppDatabase,
+    $UsersTable,
+    User,
+    $$UsersTableFilterComposer,
+    $$UsersTableOrderingComposer,
+    $$UsersTableProcessedTableManager,
+    $$UsersTableInsertCompanionBuilder,
+    $$UsersTableUpdateCompanionBuilder> {
+  $$UsersTableProcessedTableManager(super.$state);
+}
+
+class $$UsersTableFilterComposer
+    extends FilterComposer<_$AppDatabase, $UsersTable> {
+  $$UsersTableFilterComposer(super.$state);
+  ColumnFilters<int> get id => $state.composableBuilder(
+      column: $state.table.id,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+
+  ColumnFilters<String> get login => $state.composableBuilder(
+      column: $state.table.login,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+
+  ColumnFilters<String> get password => $state.composableBuilder(
+      column: $state.table.password,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+
+  ColumnFilters<int> get isLogin => $state.composableBuilder(
+      column: $state.table.isLogin,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+}
+
+class $$UsersTableOrderingComposer
+    extends OrderingComposer<_$AppDatabase, $UsersTable> {
+  $$UsersTableOrderingComposer(super.$state);
+  ColumnOrderings<int> get id => $state.composableBuilder(
+      column: $state.table.id,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+
+  ColumnOrderings<String> get login => $state.composableBuilder(
+      column: $state.table.login,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+
+  ColumnOrderings<String> get password => $state.composableBuilder(
+      column: $state.table.password,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+
+  ColumnOrderings<int> get isLogin => $state.composableBuilder(
+      column: $state.table.isLogin,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+}
+
 class _$AppDatabaseManager {
   final _$AppDatabase _db;
   _$AppDatabaseManager(this._db);
   $$FriendsTableTableManager get friends =>
       $$FriendsTableTableManager(_db, _db.friends);
+  $$UsersTableTableManager get users =>
+      $$UsersTableTableManager(_db, _db.users);
 }

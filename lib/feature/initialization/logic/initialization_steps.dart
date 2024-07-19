d40e32acb359499dc98b5b43b86b8c4d099a9f88
Stanislav Mironov <mirst33@icloud.com>
2024-07-19 17:22:28 +0300

    correction router

--- lib/feature/initialization/logic/initialization_steps.dart
+++ lib/feature/initialization/logic/initialization_steps.dart
@@ -1,18 +1,30 @@
 import 'dart:async';
 
-import 'package:friends_list/core/components/database/src/app_database.dart';
+import 'package:friends_list/core/components/database/database.dart';
+import 'package:friends_list/core/components/router/router.dart';
+import 'package:friends_list/feature/auth/data/auth_data_source.dart';
+import 'package:friends_list/feature/auth/data/auth_repository.dart';
 import 'package:friends_list/feature/home/friends/data/friend_data_source.dart';
 import 'package:friends_list/feature/home/friends/data/friend_repository.dart';
-
-import '../model/initialization_progress.dart';
+import 'package:friends_list/feature/initialization/model/initialization_progress.dart';
 
 typedef StepAction = FutureOr<void>? Function(InitializationProgress progress);
 
 mixin InitializationSteps {
   final initializationSteps = <String, StepAction>{
+    "Router": (progress) {
+      final appRouter = AppRouter();
+      return progress.dependencies.appRouter = appRouter;
+    },
     "Database": (progress) {
-      final database = AppDatabase();
-      return progress.dependencies.database = database;
+      final db = AppDatabase();
+      return progress.dependencies.database = db;
+    },
+    "AuthRepository": (progress) {
+      final authRepository = AuthRepositoryImpl(
+        AuthDao(progress.dependencies.database),
+      );
+      return progress.dependencies.authRepository = authRepository;
     },
     "FriendRepository": (progress) {
       final friendRepository =
--- lib/feature/initialization/model/dependencies.dart
+++ lib/feature/initialization/model/dependencies.dart
@@ -1,19 +1,15 @@
 import 'package:friends_list/core/components/database/database.dart';
-//import 'package:friends_list/core/router/router.dart';
-//import 'package:friends_list/feature/auth/data/auth_repository.dart';
+import 'package:friends_list/core/components/router/router.dart';
+import 'package:friends_list/feature/auth/data/auth_repository.dart';
 import 'package:friends_list/feature/home/friends/data/friend_repository.dart';
-import 'package:friends_list/feature/home/friends/logic/bloc/friends_bloc.dart';
-//import '../../auth/bloc/auth_bloc.dart';
 
 base class Dependencies {
   Dependencies();
 
   late final AppDatabase database;
-  //late final AuthRepository authRepository;
-  //late final AppRouter appRouter;
-  //late final AuthBloc authBloc;
+  late final AuthRepository authRepository;
+  late final AppRouter appRouter;
   late final FriendRepository friendRepository;
-  late final FriendBloc friendBloc;
 }
 
 final class InitializationResult {
